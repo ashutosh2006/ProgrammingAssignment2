64dedb0a63228941086e733ff94c765007d05a00
Ashutosh Tripathi <ashutosh.sns@gmail.com>
2015-10-22 15:24:36 +0530

    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,71 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## x: assuming a square invertible matrix
+  inv = NULL
+  ## Function for setting value
+  set = function(y) {
+    # assigning value to x
+    x <<- y
+    # setting inv as null
+    inv <<- NULL
+  }
+  ## Function for fetching value
+  get = function() x
+  ## Set inverse of x
+  setinv = function(inverse) inv <<- inverse 
+  ## fetch inverse of x
+  getinv = function() inv
+  
+  ## return: list containing following functions 
+  list(set=set, 
+       get=get, 
+       setinv=setinv, 
+       getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## x: result of function makeCacheMatrix()
+ 
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    message("getting cached inverse matrix")
+    ## return: inverse of matrix 
+    return(inv)
+  }
+  
+  # otherwise, calculates the inverse 
+  data = x$get()
+  
+  ## check martix is square 
+  ## can use is.square.matrix(x) function as well if matrixcalc lib is installed
+  if (nrow(data) != ncol(data)) {
+    message("not a square matrix")
+    return (NULL)
+  }
+  
+  ## install matrixcalc using --- install.package('matrixcalc')
+  ## include --- library('matrixcalc') 
+  ## above library facilitate is.singular.matrix function
+  if (is.function(is.singular.matrix) == TRUE) {
+    if (is.singular.matrix(data) == TRUE) {
+      message("matrix is singular")
+      return (NULL)
+    }
+  }
+  
+  inv = solve(data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  ## return: inverse of matrix 
+  return(inv)
+  
 }
